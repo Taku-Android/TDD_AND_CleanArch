d6d43e8e2fae0b7e8e4e7acbd384d69e4b1144cd
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-10-29 11:09:31 +0200

    after finishing the repository in the domain layer we need the failure.dart class to catch the error the repo is like a contract and the impl in data layer

---
+++ lib/domain/repository/weather_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluttter_tdd_and_clean_arch/core/error/failure.dart';
+import 'package:fluttter_tdd_and_clean_arch/domain/entities/weather_entity.dart';
+
+abstract class WeatherRepository{
+
+  Future<Either<Failure, WeatherEntity>> getCurrentWeather(String cityName);
+
+}
